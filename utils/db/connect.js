016f01b5f74fcfcd4d75abaee44483cd00c59387
ohShews <pfannenstielhunter@gmail.com>
2023-04-03 21:30:45 -0500

    Enabled cloudinary image uploads, sign-up complete except for db

--- utils/db/connect.js
+++ utils/db/connect.js
@@ -1,7 +1,7 @@
 const sql = require("msnodesqlv8");
 
 const connnection =
-  "server=(localdb)\\MSSQLLocalDb;Database=ChatApplication;Trusted_Connection=Yes;Driver={OBDC Driver 18 for SQL Server}";
+  "Server=(localdb)\\MSSQLLocalDb;Database=ChatApplication;Trusted_Connection=Yes;Driver={MS Access Database}";
 
 const query = "SELECT ethereumAddress FROM Chat.[User]";
 
