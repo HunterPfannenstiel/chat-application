c8c3d7d6c1d4c426623ce3ddc99fef189fb0b1d9
Payton Pfannenstiel <payton02@ksu.edu>
2023-04-06 20:13:12 -0500

    Basic follower list rendering

--- components/ui/Followers/Followers.tsx
+++ components/ui/Followers/Followers.tsx
@@ -1,14 +1,36 @@
-import { FollowerDetails } from '@_types/user';
-import { FunctionComponent } from 'react';
-import classes from './Followers.module.css'
-    
+import { FollowerDetails } from "@_types/user";
+import { FunctionComponent } from "react";
+import classes from "./Followers.module.css";
+import ProfileImage from "@ui/Resuable/Profile/ProfileImage/ProfileImage";
+
 interface FollowersProps {
-    followers: FollowerDetails[];
+	followers: FollowerDetails[];
 }
-    
+
 const Followers: FunctionComponent<FollowersProps> = ({ followers }) => {
-    console.log(followers);
- return ( <></> );
-}
-    
+	console.log("followers", followers);
+	return (
+		<>
+			{followers.map((follower) => {
+				return (
+					<div className={classes["follower-block"]}>
+						<div className={classes["pfp-name-container"]}>
+							<ProfileImage
+								src={follower.userImage}
+								className={classes["image-block"]}
+							/>
+							<div className={classes.name}>
+								<p>{follower.userName}</p>
+								<p>{`@${follower.userHandle}`}</p>
+							</div>
+							<button>Follow</button>
+						</div>
+						<div className={classes.description}>{follower.bio}</div>
+					</div>
+				);
+			})}
+		</>
+	);
+};
+
 export default Followers;
