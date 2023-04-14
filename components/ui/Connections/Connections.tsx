85436147f91b6820cb91499a19f82022e39b7aa4
Payton Pfannenstiel <payton02@ksu.edu>
2023-04-13 21:42:40 -0500

    Renamed and refactored Followers component

---
+++ components/ui/Connections/Connections.tsx
@@ -0,0 +1,23 @@
+import { FollowerDetails } from "@_types/user";
+import { FunctionComponent } from "react";
+import UserBlock from "@ui/Resuable/UserBlock/UserBlock";
+import classes from "components/ui/Connections/Connections.module.css"
+
+interface ConnectionsProps {
+	users: FollowerDetails[];
+	heading?: string;
+}
+
+const Connections: FunctionComponent<ConnectionsProps> = ({ users, heading}) => {
+	console.log("users", users);
+	return (
+		<>
+			<h1 className={classes.heading}>{heading}</h1>
+			{users.map((user) => {
+				return <UserBlock user={user} buttonText="Follow" />;
+			})}
+		</>
+	);
+};
+
+export default Connections;
