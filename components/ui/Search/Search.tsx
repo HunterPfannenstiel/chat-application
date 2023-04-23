52eca5d10abd9d6024e32c262796cdee366ea8bc
Payton Pfannenstiel <payton02@ksu.edu>
2023-04-23 16:25:21 -0500

    Started adding styling for desktop

--- components/ui/Search/Search.tsx
+++ components/ui/Search/Search.tsx
@@ -2,17 +2,14 @@ import { FunctionComponent } from "react";
 import useUserSearch from "@hooks/user-search/useUserSearch";
 import UserBlock from "@ui/Resuable/UserBlock/UserBlock";
 import { useSearch } from "components/providers/Search/Search";
+import Connections from "@ui/Connections/Connections";
 
 interface SearchProps {}
 
 const Search: FunctionComponent<SearchProps> = () => {
   const { users } = useSearch();
   return (
-    <ul>
-      {users.map((user) => {
-        return <UserBlock user={user} />;
-      })}
-    </ul>
+    <Connections users={users} />
   );
 };
 
