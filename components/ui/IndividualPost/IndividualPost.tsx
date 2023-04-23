7a79169936ed3d0c99a8e80a79a8e5fd2073bb78
Payton Pfannenstiel <payton02@ksu.edu>
2023-04-23 12:31:31 -0500

    Styled individual post page

--- components/ui/IndividualPost/IndividualPost.tsx
+++ components/ui/IndividualPost/IndividualPost.tsx
@@ -8,47 +8,54 @@ import { ImageInfo } from "@ui/Resuable/PostModal/types";
 import { createPost } from "utils/actions";
 
 interface IndividualPostProps {
-  mainPost: FeedPosts;
-  commentPosts: FeedPosts[];
+	mainPost: FeedPosts;
+	commentPosts: FeedPosts[];
 }
 
 const IndividualPost: FunctionComponent<IndividualPostProps> = ({
-  mainPost,
-  commentPosts,
+	mainPost,
+	commentPosts,
 }) => {
-  const [comments, setComments] = useState(commentPosts);
-  const { showModal, playAnimation, toggle } = useAnimateModal(300);
-  const addNewComment = (comment: FeedPost) => {
-    setComments((prevState) => [...prevState, comment]);
-  };
-  return (
-    <>
-      <FeedPost post={mainPost} />
-      {comments.map((post) => {
-        return <FeedPost post={post} />;
-      })}
-      <button className={classes.add_comment} onClick={toggle}>
-        Add Comment
-      </button>
-      {showModal && (
-        <PostModal
-          modalProps={{ playAnimation, toggle, animationTime: 300 }}
-          modalTitle="Create a Comment"
-          createPostHandler={createCommentHandler(
-            mainPost.postId,
-            addNewComment
-          )}
-        />
-      )}
-    </>
-  );
+	const [comments, setComments] = useState(commentPosts);
+	const { showModal, playAnimation, toggle } = useAnimateModal(300);
+	const replyCount = commentPosts.length;
+
+	const addNewComment = (comment: FeedPost) => {
+		setComments((prevState) => [...prevState, comment]);
+	};
+	return (
+		<>
+			<FeedPost post={mainPost} />
+			<div className={classes.add_comment}>
+				<button onClick={toggle}>Post a Reply</button>
+			</div>
+			<p className={classes.count}>
+				Replies: <span>{replyCount}</span>
+			</p>
+			<ul>
+				{comments.map((post) => {
+					return <FeedPost post={post} key={post.postId} />;
+				})}
+			</ul>
+			{showModal && (
+				<PostModal
+					modalProps={{ playAnimation, toggle, animationTime: 300 }}
+					modalTitle="Create a Comment"
+					createPostHandler={createCommentHandler(
+						mainPost.postId,
+						addNewComment
+					)}
+				/>
+			)}
+		</>
+	);
 };
 
 const createCommentHandler =
-  (postId: number, newComment: (comment: FeedPosts) => void) =>
-  async (content: string, images: ImageInfo[]) => {
-    const comment = await createPost(content, images, postId);
-    newComment(comment);
-  };
+	(postId: number, newComment: (comment: FeedPosts) => void) =>
+	async (content: string, images: ImageInfo[]) => {
+		const comment = await createPost(content, images, postId);
+		newComment(comment);
+	};
 
 export default IndividualPost;
